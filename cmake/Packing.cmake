f66e5aa31afd6358eeaa1ce3e4117841c0c237a4
Joe Dong <jian.dong@pobox.com>
2023-05-25 14:03:00 +0800

    update package description

--- cmake/Packing.cmake
+++ cmake/Packing.cmake
@@ -1,11 +1,9 @@
 # these are cache variables, so they could be overwritten with -D,
-set(CPACK_PACKAGE_NAME ${PROJECT_NAME}
-    CACHE STRING "The resulting package name"
-)
+set(CPACK_PACKAGE_NAME ${PROJECT_NAME})
 # which is useful in case of packing only selected components instead of the whole thing
-set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Simple C++ application"
-    CACHE STRING "Package description for the package metadata"
-)
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY " OrbbecSDK is a comprehensive library that provides developers with intuitive interfaces to control and
+        manipulate Orbbec cameras seamlessly")
+
 set(CPACK_PACKAGE_VENDOR "orbbec")
 
 
@@ -22,8 +20,8 @@ set(CPACK_PACKAGE_VERSION_MINOR ${PROJECT_VERSION_MINOR})
 set(CPACK_PACKAGE_VERSION_PATCH ${PROJECT_VERSION_PATCH})
 set(CPACK_DEBIAN_PACKAGE_DEPENDS "libudev1")
 
-set(CPACK_PACKAGE_CONTACT "YOUR@E-MAIL.net")
-set(CPACK_DEBIAN_PACKAGE_MAINTAINER "YOUR NAME")
+set(CPACK_PACKAGE_CONTACT "developer@orbbec.com")
+set(CPACK_DEBIAN_PACKAGE_MAINTAINER "Orbbec Developer")
 
 set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/LICENSE.txt")
 set(CPACK_RESOURCE_FILE_README "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
@@ -34,7 +32,7 @@ set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
 # that is if you want every group to have its own package,
 # although the same will happen if this is not set (so it defaults to ONE_PER_GROUP)
 # and CPACK_DEB_COMPONENT_INSTALL is set to YES
-set(CPACK_COMPONENTS_GROUPING ALL_COMPONENTS_IN_ONE)#ONE_PER_GROUP)
+set(CPACK_COMPONENTS_GROUPING ALL_COMPONENTS_IN_ONE)
 # without this you won't be able to pack only specified component
 set(CPACK_DEB_COMPONENT_INSTALL YES)
 
